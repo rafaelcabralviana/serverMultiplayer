ad404a60b4af579b1dc88c96b854a31752f96fcc
Rafael C Viana <30372591+rafaelcabralviana@users.noreply.github.com>
2024-01-13 11:03:16 -0300

    second commit

---
+++ backend/src/server ultimo funcionando.js
@@ -0,0 +1,66 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+// Armazena os objetos associados aos clientes usando o próprio codID como chave
+const objetos = {};
+
+wss.on('connection', (ws, req) => {
+  
+  
+  // Extrai o codID da URL da requisição
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const codID = url.searchParams.get('codID');
+  const ip = req.socket.remoteAddress;
+  
+
+  console.log('Nova Conexão Cliente ID: ', codID ,' IP: ', ip);
+
+  // Verifica se o codID está presente
+  if (codID) {
+    // Associa o objeto ao cliente usando o codID como chave
+    objetos[codID] = { webSocket: ws };
+
+    ws.on('message', (message) => {
+      try {
+        const dadosUsuario = JSON.parse(message);
+
+        //console.log('Coordenadas recebidas:', dadosUsuario);
+
+        // Repassa a mensagem para todos os clientes conectados
+        wss.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(message);
+          }
+        });
+      } catch (error) {
+        console.error('Erro ao analisar as coordenadas:', error);
+      }
+    });
+
+    // Evento para tratar desconexão do cliente
+    ws.on('close', (code, reason) => {
+      console.log(`Cliente desconectado. ID: ${codID}`);
+
+      // Envia uma mensagem informando a desconexão do cliente para todos os clientes
+      const mensagemDesconexao = JSON.stringify({
+        tipo: 'desconexao',
+        codID: codID
+      });
+
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(mensagemDesconexao);
+        }
+      });
+
+    });
+
+    ws.on('error', (error) => {
+      console.error('Erro WebSocket:', error);
+    });
+  } else {
+    //console.error('URL da requisição não contém codID. Encerrando conexão.');
+    //ws.close(1002, 'URL da requisição não contém codID');
+  }
+});
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,12 +5,16 @@ const wss = new WebSocket.Server({ port: 8080 });
 // Armazena os objetos associados aos clientes usando o próprio codID como chave
 const objetos = {};
 
+// Lista para armazenar dados do usuário (nomeUser, codID e última coordenada)
+const dadosUsuarios = [];
+
 wss.on('connection', (ws, req) => {
-  //console.log('Novo cliente conectado');
-  
   // Extrai o codID da URL da requisição
   const url = new URL(req.url, `http://${req.headers.host}`);
   const codID = url.searchParams.get('codID');
+  const ip = req.socket.remoteAddress;
+
+  console.log('Nova Conexão Cliente ID:', codID, ' IP:', ip);
 
   // Verifica se o codID está presente
   if (codID) {
@@ -20,7 +24,25 @@ wss.on('connection', (ws, req) => {
     ws.on('message', (message) => {
       try {
         const dadosUsuario = JSON.parse(message);
-        console.log('Coordenadas recebidas:', dadosUsuario);
+        //console.log(dadosUsuario);
+        // Atualiza a lista de dados do usuário
+        const index = dadosUsuarios.findIndex((user) => user.codID === codID);
+        if (index !== -1) {
+          dadosUsuarios[index] = {
+            nomeUser: dadosUsuario.nomeUser,
+            codID: codID,
+            ultimaCoordenada: { x: dadosUsuario.x, y: dadosUsuario.y, z: dadosUsuario.z },
+            ultimaRotacao: { rotX: dadosUsuario.rotX, rotY: dadosUsuario.rotY, rotZ: dadosUsuario.rotZ },
+          };
+        } else {
+          dadosUsuarios.push({
+            nomeUser: dadosUsuario.nomeUser,
+            codID: codID,
+            ultimaCoordenada: { x: dadosUsuario.x, y: dadosUsuario.y, z: dadosUsuario.z },
+            ultimaRotacao: { rotX: dadosUsuario.rotX, rotY: dadosUsuario.rotY, rotZ: dadosUsuario.rotZ },
+          });
+        }
+        
 
         // Repassa a mensagem para todos os clientes conectados
         wss.clients.forEach((client) => {
@@ -33,14 +55,28 @@ wss.on('connection', (ws, req) => {
       }
     });
 
+    // Envia os dados do usuário para o novo cliente
+    const dadosUsuarioAtualizados = JSON.stringify({
+      //tipo: 'dadosUsuario',
+      listaDadosUsuarios: dadosUsuarios,
+    });
+
+    ws.send(dadosUsuarioAtualizados);
+
     // Evento para tratar desconexão do cliente
     ws.on('close', (code, reason) => {
-      console.log(`Cliente desconectado. CodID: ${codID}`);
+      console.log(`Cliente desconectado. ID: ${codID}`);
+
+      // Remove os dados do usuário da lista ao se desconectar
+      const index = dadosUsuarios.findIndex((user) => user.codID === codID);
+      if (index !== -1) {
+        dadosUsuarios.splice(index, 1);
+      }
 
       // Envia uma mensagem informando a desconexão do cliente para todos os clientes
       const mensagemDesconexao = JSON.stringify({
         tipo: 'desconexao',
-        codID: codID
+        codID: codID,
       });
 
       wss.clients.forEach((client) => {
@@ -48,14 +84,13 @@ wss.on('connection', (ws, req) => {
           client.send(mensagemDesconexao);
         }
       });
-
     });
 
     ws.on('error', (error) => {
       console.error('Erro WebSocket:', error);
     });
   } else {
-    //console.error('URL da requisição não contém codID. Encerrando conexão.');
-    //ws.close(1002, 'URL da requisição não contém codID');
+    console.error('URL da requisição não contém codID. Encerrando conexão.');
+    ws.close(1002, 'URL da requisição não contém codID');
   }
 });
